e89ff474a44efd693445714caa6c0dffa51efda5
Kareem <kareem.mohamed9711@gmail.com>
2020-03-30 03:36:52 +0200

    LoginActivity moved to LoginFragment

--- app/src/main/java/com/example/spotify/IntroActivity.java
+++ app/src/main/java/com/example/spotify/IntroActivity.java
@@ -5,22 +5,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class IntroActivity extends AppCompatActivity {
+
+    private static Retrofit retrofit;
+    private static ApiSpotify apiSpotify;
+    private static LoginFragment loginFragment = null;
+    //private static String token = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        retrofit = new Retrofit.Builder().baseUrl("http://192.168.1.39:3000").addConverterFactory(GsonConverterFactory.create()).build();
+
+        apiSpotify = retrofit.create(ApiSpotify.class);
     }
 
-    public void LaunchLoginActivity(View view) {
-        Intent intent = new Intent(this,LoginActivity.class);
-        startActivity(intent);
+
+    public void showLoginFragment(View view) {
+        loginFragment = new LoginFragment(retrofit,apiSpotify);
+        hideIntroLayout();
+        getSupportFragmentManager().beginTransaction().replace(R.id.intro_fragment,loginFragment).commit();
     }
 
     public void LaunchSignUpActivity(View view) {
         Intent intent = new Intent(this,SignUpActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(loginFragment != null){
+            getSupportFragmentManager().beginTransaction().remove(loginFragment).commit();
+            loginFragment = null;
+            showIntroLayout();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+
+    public void hideIntroLayout(){
+        findViewById(R.id.intro).setVisibility(View.GONE);
+    }
+    public void showIntroLayout(){
+        findViewById(R.id.intro).setVisibility(View.VISIBLE);
     }
 }
---
+++ app/src/main/java/com/example/spotify/LoginFragment.java
@@ -0,0 +1,91 @@
+package com.example.spotify;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+
+
+public class LoginFragment extends Fragment {
+
+    private static Retrofit mRetrofit;
+    private static ApiSpotify mApiSpotify;
+    private static String token;
+
+
+    /*public LoginFragment() {
+        // Required empty public constructor
+    }*/
+
+    public LoginFragment(Retrofit retrofit,ApiSpotify apiSpotify){
+        mRetrofit = retrofit;
+        mApiSpotify = apiSpotify;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        rootView.findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        return rootView;
+    }
+
+    public void login(){
+        String email = ((EditText) getView().findViewById(R.id.email)).getText().toString();
+        String password = ((EditText) getView().findViewById(R.id.password)).getText().toString();
+
+        LoginCredentials loginCredentials = new LoginCredentials();
+        loginCredentials.setEmail(email);
+        loginCredentials.setPassword(password);
+
+        Call<Token> call = mApiSpotify.login(loginCredentials);
+
+        call.enqueue(new Callback<Token>() {
+            @Override
+            public void onResponse(Call<Token> call, Response<Token> response) {
+                if(response.isSuccessful()){
+                    token = response.body().getToken();
+                    Toast.makeText(getContext(),"Sucess " + response.code(),Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getContext(),"Failed to login",Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Token> call, Throwable t) {
+                Log.e("Login Activity",t.getMessage());
+                Toast.makeText(getContext(),"Failed to connect",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
