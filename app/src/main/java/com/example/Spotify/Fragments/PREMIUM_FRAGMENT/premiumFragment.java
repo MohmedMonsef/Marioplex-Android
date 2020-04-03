602adc26cc292e053ee51b21989bebd18468b8b9
Kareem <kareem.mohamed9711@gmail.com>
2020-04-03 06:11:37 +0200

    Package name changed to Spotify

---
+++ app/src/main/java/com/example/Spotify/Fragments/PREMIUM_FRAGMENT/premiumFragment.java
@@ -0,0 +1,57 @@
+package com.example.Spotify.Fragments.PREMIUM_FRAGMENT;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import android.app.Fragment;
+
+//import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.Spotify.Activities.MainActivity;
+import com.example.Spotify.R;
+
+import static android.widget.Toast.makeText;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * to handle interaction events.
+ * create an instance of this fragment.
+ */
+public class premiumFragment extends Fragment implements LifecycleOwner {
+
+    private viewmodelPremium premiumViewModel;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_premium, container, false);
+        premiumViewModel =ViewModelProviders.of((MainActivity) getActivity()).get(viewmodelPremium.class);
+        final TextView textView = view.findViewById(R.id.text_premium);
+        ////*******************************RecyclerView***********************////
+        return view;
+
+    }
+
+
+
+
+    @NonNull
+    @Override
+    public Lifecycle getLifecycle() {
+        return null;
+    }
+}
